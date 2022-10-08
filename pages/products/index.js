113b40c88b748c11fb24d52e104a529135f93549
AyonC <e10901ayon@dpsrkp.net>
2022-10-08 10:57:46 +0530

    complet: products page

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,6 +1,18 @@
+import productsStyles from '../../styles/Products.module.css'
+
+import ProductData from '../../data/ProductData'
+import ProductCard from '../../components/Product/ProductCard'
+
 function products() {
   return (
-    <h1>Products</h1>
+    <>
+      <div className={productsStyles.products}>
+        <h1>Our Products</h1>
+        <div className={productsStyles.cards}>
+          <ProductCard data={ProductData}/>
+        </div>
+      </div>
+    </>
   )
 }
 
