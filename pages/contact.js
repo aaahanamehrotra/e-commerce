b57d12afe66f89ff0029130eba2ae61aa71c0600
aaahanamehrotra <e10991aaahana@dpsrkp.net>
2022-10-10 21:42:13 +0530

    auth

---
+++ pages/contact.js
@@ -0,0 +1,47 @@
+import contactStyles from "../styles/Contact.module.css";
+import { useSession } from "next-auth/react";
+import Navbar from "../components/Navbar/Navbar";
+
+function Contact() {
+  const { data: session } = useSession();
+  return (
+    <>
+      <Navbar session={session} />
+      <div className={contactStyles.contact}>
+        <h1>Contact Us</h1>
+        <div className={contactStyles.form}>
+          <form>
+            <div className={contactStyles.inputGroup}>
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" placeholder="John Doe" />
+            </div>
+            <div className={contactStyles.inputGroup}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="johndoe@gmail.com"
+              />
+            </div>
+            <div className={contactStyles.inputGroup}>
+              <label htmlFor="textarea">Yours Message</label>
+              <textarea
+                name="textarea"
+                cols="30"
+                rows="10"
+                placeholder="Your message"
+              ></textarea>
+            </div>
+            <div className={contactStyles.inputGroup}>
+              <button type="submit" className={contactStyles.button}>
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
